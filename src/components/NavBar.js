52e69f78c8e7ed96cb30e4a3d484f55d237dcba8
humaid123 <61509402+humaid123@users.noreply.github.com>
2021-04-25 05:48:13 -0300

    navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,59 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {
+  FaHome,
+  FaCode,
+  FaBriefcase,
+  FaLightbulb,
+  FaFeatherAlt,
+  FaShareAlt,
+  FaGreaterThan,
+} from "react-icons/fa";
 
 export default function NavBar() {
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/skills">Skills</Link>
-      <Link to="/work">Work Experience</Link>
-      <Link to="/projects">Projects</Link>
-      <Link to="/education">Education</Link>
-      <Link to="/contacts">Contacts</Link>
-    </div>
+    <nav class="navbar">
+      <ul class="navbar-nav">
+        <li class="logo">
+          <FaGreaterThan />
+        </li>
+        <li class="nav-item">
+          <Link className="nav-link" to="/">
+            <FaHome className="icon" />
+            <span class="link-text">Home</span>
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/skills">
+            <FaCode className="icon" />
+            <span class="link-text">Skills</span>
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/work">
+            <FaBriefcase className="icon" />
+            <span class="link-text">Work Experience</span>
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/projects">
+            <FaLightbulb className="icon" />
+            <span class="link-text">Projects</span>
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/education">
+            <FaFeatherAlt className="icon" />
+            <span class="link-text">Education</span>
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/contacts">
+            <FaShareAlt className="icon" />
+            <span class="link-text">Contacts</span>
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const home_style = {
   display: "flex",
-  width: "100vw",
-  height: "100vw",
-  margin: "auto",
+  width: "90vw",
+  height: "100vh",
+  alignItems: "center",
+  justifyContent: "center",
+  flexDirection: "column",
 };
 
+const name_style = { padding: 0, margin: 0, marginLeft: 20, fontSize: "5rem" };
+
+const title_style = { marginLeft: "10rem", fontSize: "2rem" };
 export default function Home() {
   return (
-    <div style={home_style}>
-      <h1>Humaid M. Agowun</h1>
-      <h2>Student, Developer and Researcher</h2>
+    <div className="home" style={home_style}>
+      <p style={name_style}>Humaid M. Agowun</p>
+      <p style={title_style}>Developer, Student, Researcher</p>
     </div>
   );
 }
