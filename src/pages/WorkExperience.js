189cfca237cc81be663c6476bd37c53802a99ba7
humaid123 <61509402+humaid123@users.noreply.github.com>
2021-04-26 18:26:51 -0300

    Work experience page animated

--- src/pages/WorkExperience.js
+++ src/pages/WorkExperience.js
@@ -1,39 +1,59 @@
 import React from "react";
 
-function BIResearchAssistant() {
+import { FaChevronCircleRight } from "react-icons/fa";
+
+function BIResearchAssistant({ order }) {
   return (
-    <div>
-      <h1>Research Assitant - Business Intelligence</h1>
-      <p>
-        Optimised segmented linear regression algorithm with Dynamic Programming
-      </p>
-      <p>
-        Used Python (Numpy) and Java (Apache Common Maths) to implement final
-        version
-      </p>
-      <p>Supervised by Dr. Hai Wang</p>
+    <div className="entrance" style={{ "--animation-order": order }}>
+      <div className="workCard">
+        <h1>Research Assitant - Business Intelligence</h1>
+        <p>
+          <FaChevronCircleRight style={{ fontSize: "0.8rem" }} />
+          {"   "}
+          Optimised segmented linear regression algorithm with Dynamic
+          Programming
+        </p>
+        <p>
+          <FaChevronCircleRight style={{ fontSize: "0.8rem" }} />
+          {"   "}
+          Used Python (Numpy) and Java (Apache Common Maths) to implement final
+          version
+        </p>
+        <p>
+          <FaChevronCircleRight style={{ fontSize: "0.8rem" }} />
+          {"   "}
+          Supervised by Dr. Hai Wang
+        </p>
+      </div>
     </div>
   );
 }
 
-function ITSecurityIntern() {
+function ITSecurityIntern({ order }) {
   return (
-    <div>
-      <h1>IT Security intern</h1>
-      <p>Obtained Security incidence response Training</p>
-      <p>
-        Main Project was to create data visualisation tool to improve resource
-        allocations
-      </p>
+    <div className="entrance" style={{ "--animation-order": order }}>
+      <div className="workCard">
+        <h1>IT Security intern</h1>
+        <p>
+          <FaChevronCircleRight style={{ fontSize: "0.8rem" }} />
+          {"   "}Obtained Security incidence response Training
+        </p>
+        <p>
+          <FaChevronCircleRight style={{ fontSize: "0.8rem" }} />
+          {"   "}
+          Main Project was to create data visualisation tool to improve resource
+          allocations
+        </p>
+      </div>
     </div>
   );
 }
 
 export default function WorkExperience() {
   return (
-    <div>
-      <BIResearchAssistant />
-      <ITSecurityIntern />
+    <div className="workMain">
+      <BIResearchAssistant order={2} />
+      <ITSecurityIntern order={1} />
     </div>
   );
 }
