8b25010277422644b0d5e8b0b9f396512375ab66
humaid123 <61509402+humaid123@users.noreply.github.com>
2021-04-26 17:15:44 -0300

    skills page animated and styled

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,15 +1,15 @@
 import React from "react";
 
 function High() {
-  return <div style={{ backgroundColor: "green", width: "5em" }} />;
+  return <div style={{ background: "#03DAC6", width: "80%", height: "1em" }} />;
 }
 
 function Medium() {
-  return <div style={{ BackgroundColor: "orange", width: "4em" }} />;
+  return <div style={{ background: "#BB86FC", width: "60%", height: "1em" }} />;
 }
 
 function Introductory() {
-  return <div style={{ BackgroundColor: "yellow", width: "3em" }} />;
+  return <div style={{ background: "#3700B3", width: "40%", height: "1em" }} />;
 }
 
 function Row(props) {
@@ -18,22 +18,27 @@ function Row(props) {
   else if (props.level === "Medium") level = <Medium />;
   else if (props.level === "Introductory") level = <Introductory />;
   return (
-    <div>
+    <div style={{ display: "grid", gridTemplateColumns: "30% 70%" }}>
       <div className="item">{props.text}</div>
-      <div className="level">{level}</div>
+      <div
+        className="level skillsRow"
+        style={{ "--animation-order": props.order }}
+      >
+        {level}
+      </div>
     </div>
   );
 }
 
 function Languages() {
   return (
-    <div>
+    <div className="skillsCard">
       <h1>Languages</h1>
-      <div className="table">
-        <Row text="JavaScript (ES6+)" level="High" />
-        <Row text="Python" level="High" />
-        <Row text="C++" level="Medium" />
-        <Row text="Java" level="Introductory" />
+      <div>
+        <Row text="JavaScript (ES6+)" order={1} level="High" />
+        <Row text="Python" order={2} level="High" />
+        <Row text="C++" order={3} level="Medium" />
+        <Row text="Java" order={4} level="Introductory" />
       </div>
     </div>
   );
@@ -41,15 +46,15 @@ function Languages() {
 
 function WebDevelopment() {
   return (
-    <div>
+    <div className="skillsCard">
       <h1>Web Development</h1>
-      <div className="table">
-        <Row text="React" level="High" />
-        <Row text="MERN" level="High" />
-        <Row text="jQuery" level="High" />
-        <Row text="TypeScript" level="Medium" />
-        <Row text="Sass" level="Medium" />
-        <Row text="Firebase" level="Introductory" />
+      <div>
+        <Row text="React" order={1} level="High" />
+        <Row text="MERN" order={2} level="High" />
+        <Row text="jQuery" order={3} level="High" />
+        <Row text="TypeScript" order={4} level="Medium" />
+        <Row text="Sass" order={5} level="Medium" />
+        <Row text="Firebase" order={6} level="Introductory" />
       </div>
     </div>
   );
@@ -57,14 +62,14 @@ function WebDevelopment() {
 
 function MachineLearning() {
   return (
-    <div>
+    <div className="skillsCard">
       <h1>Data Analysis and Machine Learning</h1>
-      <div className="table">
-        <Row text="Numpy/Scipy" level="High" />
-        <Row text="Pandas" level="High" />
-        <Row text="Matplotlib" level="High" />
-        <Row text="Scikit Learn" level="Medium" />
-        <Row text="TensorFlow" level="Introductory" />
+      <div>
+        <Row text="Numpy/Scipy" order={1} level="High" />
+        <Row text="Pandas" order={2} level="High" />
+        <Row text="Matplotlib" order={3} level="High" />
+        <Row text="Scikit Learn" order={4} level="Medium" />
+        <Row text="TensorFlow" order={5} level="Introductory" />
       </div>
     </div>
   );
@@ -72,12 +77,12 @@ function MachineLearning() {
 
 function OtherSkills() {
   return (
-    <div>
+    <div className="skillsCard">
       <h1>Other Strong Skills</h1>
-      <div className="table">
-        <Row text="OOP" level="High" />
-        <Row text="SQL" level="High" />
-        <Row text="Matlab/Scilab" level="Medium" />
+      <div>
+        <Row text="OOP" order={1} level="High" />
+        <Row text="SQL" order={2} level="High" />
+        <Row text="Matlab/Scilab" order={3} level="Medium" />
       </div>
     </div>
   );
@@ -85,8 +90,8 @@ function OtherSkills() {
 
 function SoftSkills() {
   return (
-    <div>
-      <h1>Important Soft Skills</h1>
+    <div className="skillsAdditionalInfo">
+      <h1>Skills</h1>
       <p>
         Strong <em>problem solving skills</em> being a Maths major and doing
         research.
@@ -105,12 +110,14 @@ function SoftSkills() {
 
 export default function Skills() {
   return (
-    <div>
-      <Languages />
-      <WebDevelopment />
-      <MachineLearning />
-      <OtherSkills />
+    <div style={{ width: "95%" }}>
       <SoftSkills />
+      <div className="skillsLayout">
+        <Languages />
+        <WebDevelopment />
+        <MachineLearning />
+        <OtherSkills />
+      </div>
     </div>
   );
 }
