5a6e98af2bf3cb1ea26949481a7785de33869392
humaid123 <huagowun@gmail.com>
2022-08-06 11:03:07 -0700

    Education updated for Aug 2022

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,30 @@
 import React from "react";
 import { FaChevronCircleRight } from "react-icons/fa";
 
-function Card({ title, where, description, bullets }) {
+function Card({ title, where, description, bullets, importantNote }) {
   return (
     <div className="card">
       <h1>
         <em>{title}</em>
       </h1>
       <h3>{where}</h3>
+      {importantNote && (
+        <p
+          style={{
+            color: "var(--bg-primary)",
+            padding: "5px",
+            borderRadius: "5px",
+            backgroundColor: "var(--highlight)",
+            width: "100%",
+            fontWeight: "bold",
+          }}
+        >
+          {importantNote}
+        </p>
+      )}
       {description && <p>{description}</p>}
       {bullets.map((bullet, index) => (
-        <p key={index}>
+        <p key={`${title}_${index}`}>
           <FaChevronCircleRight
             style={{
               fontSize: "0.8rem",
--- src/pages/Education.js
+++ src/pages/Education.js
@@ -9,13 +9,15 @@ function Education() {
       <Card
         title={"Bsc - Double Honours Maths and Computer Science"}
         where={"Saint Mary's University, Halifax, NS"}
+        importantNote={"Graduated May 2022"}
         description={
-          "I am a COOP Double Honours in Computer Science and Mathematics Student. My GPA is 4.28/4.3 and my notable accomplishments at SMU are: "
+          "I was a COOP Double Honours in Computer Science and Mathematics Student. My GPA is 4.28/4.3 and my notable accomplishments at SMU are: "
         }
         bullets={[
+          "I graduated summa cum laude and received the Faculty of Science - Gold Medal, an award given to the 2022 graduate with the highest academic achievement.",
           "SMU Presidential Scholarship, which is the major entrance scholarship at SMU based on high school grades and extracurriculars.",
           "I was placed on the Dean's list every year at SMU.",
-          "I received Research Awards for research during the summer term of my Second and Third year.",
+          "I received Research Awards for research during the summer term of my Second, Third and Fourth year.",
         ]}
       />
       <Card
@@ -45,6 +47,41 @@ function Research() {
         ]}
       />
       <Card
+        title={
+          "Performance Analysis of ODE Solvers on Covid-19 Models with Discontinuities"
+        }
+        where={"Saint Mary's University"}
+        importantNote={"Published Tech Report"}
+        description={"Supervised by Dr. Paul Muir"}
+        bullets={[
+          "Further exploration of discontinous Covid-19 ODE models where we solve some of the issues discovered in my honors thesis and model a discontinuity using a sharply changing function (sigmoid and inverse sigmoid) instead of a sharp cutoff if-statement.",
+        ]}
+      />
+      <Card
+        title={
+          "Further exploration into using Hermite-Birkhoff interpolants for continuous numerical differential equation solvers"
+        }
+        where={"Saint Mary's University"}
+        description={"Supervised by Dr. Paul Muir"}
+        bullets={[
+          "Extension of previous work of using defect control with Hermite-Birkhoff interpolants by instead equiping a Runge-Kutta methods with two interpolants and doing error control based on the difference between them",
+          "Also developed a 10th order interpolant and discussed the tradeoffs and problems of using this method as opposed to traditional CRKs.",
+        ]}
+      />
+      <Card
+        title={
+          "Performance analysis on covid-19 models with discontinuities and efficient defect control for initial value ODE solvers"
+        }
+        where={"Saint Mary's University"}
+        importantNote={"Honors Thesis"}
+        description={"Supervised by Dr. Paul Muir"}
+        bullets={[
+          "My honors thesis consisted of two parts:",
+          "The first part was summarising the research into improving the accuracy and efficiency of ODE and PDE solvers for solving Covid-19 models.",
+          "The second part was abou providing continuous solutions to numerical differential equations by using Hermite interpolants for defect control.",
+        ]}
+      />
+      <Card
         title={"IVODE solver with defect control"}
         where={"Saint Mary's University"}
         description={"Supervised by Dr. Paul Muir"}
@@ -53,6 +90,7 @@ function Research() {
           "Coded a 6th and 8th order Runge-Kutta solver with interpolants of 6th and 8th order to show defect control capabilities of this new method as a proof of concept.",
         ]}
       />
+
       <Card
         title={"Covid-19 modelling with PDEs"}
         where={"Saint Mary's University"}
@@ -71,6 +109,7 @@ function Research() {
           "Looked into discontinuity problems in ODES and how they affect Covid-19 models.",
           "Did a survey of popular ODE solvers in Python, R, Scilab and Matlab on how they react to discontinuities.",
           "Discussed methods to improve the accuracy and performance of these popular ODE solvers with event detection.",
+          "Later on",
         ]}
       />
       <Card
